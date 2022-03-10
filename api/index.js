81224ed9e9a1eb9f803db274602e4395337ca8cb
Jordan Borja <www.jrdnbrj@gmail.com>
2022-03-09 23:44:46 -0500

    Create User Form

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
-const { MongoClient } = require('mongodb')
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
 const { allUsers, newUser, usersFromApi } = require('./src/userResolver')
 
-require('dotenv').config()
+
+app.use(express.json()) 
+
+const cors = require('cors')
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,36 +21,27 @@ app.get('/', (req, res) => {
 
 app.get('/users', async (req, res) => {
   const users = await allUsers()
-  console.log('users:', users)
+  // console.log('users:', users)
   res.json(users)
 })
 
-app.get('/user/create', (req, res) => {
-  res.send('Usuario!')
-  newUser()
+app.post('/user/create', (req, res) => {
+  console.log('req.body:', req.body)
+  // res.send('Usuario!')
+  // newUser()
 })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
-async function main() {
+
+async function dbConnection() {
   await mongoose.connect(process.env.MONGODB_URI)
   console.log('Connected to DB.')
 }
 
-main()
+dbConnection()
   .then(console.log)
   .catch(console.error)
   
-// const client = new MongoClient(process.env.MONGODB_URI)
-
-// async function dbConnection() {
-//   await client.connect()
-//   console.log('Connected to DB.')
-// }
-
-// dbConnection()
-//  .then(console.log)
-//  .catch(console.error)
-//  .finally(() => client.close())
